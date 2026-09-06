4f1d740b05a94e9c822285f54c4c6420d36ef559
Veerendra Soni <veerendrasoni0555@gmail.com>
2026-09-06 11:47:35 +0000

    chore(backend): setup jest testing configuration and ts-jest presets

---
+++ backend/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  forceExit: true,
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true,
+};
